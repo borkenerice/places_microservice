5ba8bb71309ded1ff152b3fb0102651b87250ade
borken <erice.borja@hotmail.com>
2019-06-28 11:16:05 +0200

    better error handling

--- api/places.py
+++ api/places.py
@@ -1,5 +1,4 @@
-from flask import make_response, abort
-from sqlalchemy.exc import IntegrityError
+from flask import abort
 import logging
 
 from config import db
@@ -25,29 +24,27 @@ def get_place(place_id):
 def update_place(place_id, place_data):
     place = Place.query.get_or_404(place_id, description=f'Place not found with the id: {place_id}')
     place_schema = PlaceSchema()
-    updated_place = place_schema.load(place_data, session=db.session).data
-    updated_place.place_id = place.place_id
-    db.session.merge(updated_place)
     try:
+        updated_place = place_schema.load(place_data, session=db.session).data
+        updated_place.place_id = place.place_id
+        db.session.merge(updated_place)
         db.session.commit()
         data = place_schema.dump(updated_place).data
         return data
-    except IntegrityError as i:
-        logger.error(f'IntegrityError: {i}')
-        abort(500, f'Place: {place_id} could not be updated')
+    except ValueError as v:
+        abort(400, f'Place: {place_id} could not be updated: {v}')
 
 
 def post_place(place_data):
-    place = Place.query.filter(Place.name == place_data.get('name')).one_or_none()
-    if place is None:
+    try:
         schema = PlaceSchema()
         new_place = schema.load(place_data, session=db.session).data
         db.session.add(new_place)
         db.session.commit()
         data = schema.dump(new_place).data
         return data, 201
-    else:
-        abort(409, f"Place: {place_data.get('name')} already exists")
+    except ValueError as v:
+        abort(400, f'Place could not be created: {v}')
 
 
 def delete_place(place_id):

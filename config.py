2048890191b37f90afdebb01b034732c99f0a3b9
Borken <berice@aliasrobotics.com>
2019-07-05 18:23:10 +0200

    modify config to use postgre

--- config.py
+++ config.py
@@ -5,14 +5,13 @@ TESTING = True
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-# Build the Sqlite ULR for SqlAlchemy
-if os.name == 'nt':
-    sqlite_url = "sqlite:///" + os.path.join(BASE_DIR, "places.db")
-else:
-    sqlite_url = "sqlite:////" + os.path.join(BASE_DIR, "places.db")
+POSTGRES_USER = 'places_micro'
+POSTGRES_PASSWORD = 'places_micro'
+POSTGRES_DB = 'places_micro_db'
+POSTGRES_SERVICE = 'postgres_places'
+SQLALCHEMY_DATABASE_URI = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVICE}:5432/{POSTGRES_DB}'
 
 SQLALCHEMY_ECHO = True
-SQLALCHEMY_DATABASE_URI = sqlite_url
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 SWAGGER_DIR = os.path.join(BASE_DIR, 'api', 'swagger.yml')

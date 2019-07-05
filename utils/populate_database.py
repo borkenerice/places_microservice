960f97cfa27668f79545806603093e44a9b476c3
Borken <berice@aliasrobotics.com>
2019-07-05 18:27:42 +0200

    modify to work inside container

--- utils/populate_database.py
+++ utils/populate_database.py
@@ -1,34 +1,48 @@
 import os
+import sys
+
+if os.path.abspath(os.curdir) not in sys.path:
+    print('...missing directory in PYTHONPATH... added!')
+    sys.path.append(os.path.abspath(os.curdir))
+
 import config
 from api import db, create_app
-from api.models import Place
+from api.models import Character
+
 
 # Data to initialize database with
-PLACES = [
+CHARACTERS = [
     {
-        "name": "Westeros",
+        "name": "Jon",
+        "place_id": "1",
+        "king": False,
+        "alive": False
     },
     {
-        "name": "Kingslanding"
+        "name": "Sansa",
+        "place_id": "1",
+        "king": True,
+        "alive": True
     },
     {
-        "name": "Winterfell"
+        "name": "Doggie",
+        "place_id": "2",
+        "king": False,
+        "alive": False
     },
 ]
 
 
 def create_database():
-    # Delete database file if it exists currently
-    if os.path.exists(os.path.join(config.BASE_DIR, 'places.db')):
-        os.remove(os.path.join(config.BASE_DIR, 'places.db'))
     # Create the database
     app = create_app()
     with app.app_context():
         db.drop_all()
         db.create_all()
         # iterate over the PEOPLE structure and populate the database
-        for place in PLACES:
-            p = Place(name=place.get("name"))
+        for character in CHARACTERS:
+            p = Character(name=character.get("name"), place_id=character.get("place_id"), king=character.get("king"),
+                          alive=character.get("alive"))
             db.session.add(p)
         db.session.commit()
 
